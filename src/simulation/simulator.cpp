1b5974e30816e5482abc93b099b792c19e5480d2
Kjell Morgenstern <kjell@fritzing.org>
2025-12-28 10:40:28 +0100

    Use Noto Sans for schematic multimeter display

--- src/simulation/simulator.cpp
+++ src/simulation/simulator.cpp
@@ -662,8 +662,11 @@ void Simulator::updateMultimeterScreen(ItemBase * multimeter, QString msg){
 	QGraphicsTextItem * schScreen = new QGraphicsTextItem(msg, multimeter);
 	schScreen->setPos(QPointF(10,10));
 	schScreen->setZValue(std::numeric_limits<double>::max());
-	QFont font("Segment16C", 10, QFont::Normal);
-	bbScreen->setFont(font);
+	QFont seg_font("Segment16C", 10, QFont::Normal);
+	bbScreen->setFont(seg_font);
+	QFont noto_font("Noto Sans", 10, QFont::Normal);
+	schScreen->setFont(noto_font);
+
 	//There are issues as the size of the text changes depending on the display settings in windows
 	//This hack scales the text to match the appropiate value
 	QRectF bbMultBoundingBox = m_sch2bbItemHash.value(multimeter)->boundingRect();
